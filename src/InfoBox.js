70d65743e766c0c6e32fadb159b68acee2a19b32
David Paucar <soydavidpaucar@gmail.com>
2021-11-07 23:14:49 -0500

    :art: coronavirus card
    created initial coronavirus cards

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,8 +1,18 @@
-const InfoBox = () => {
+import { Card, CardContent, Typography } from '@mui/material';
+
+const InfoBox = ({ title, cases, total }) => {
 	return (
-		<div>
-		
-		</div>
+		<Card className="infoBox">
+			<CardContent>
+				<Typography className="infoBox__title" color="text.secondary">{title}</Typography>
+				
+				<h2 className="infoBox__cases">{cases}</h2>
+				
+				<Typography className="infoBox__total" color='text.secondary'>
+					{total} Total
+				</Typography>
+			</CardContent>
+		</Card>
 	);
 };
 
