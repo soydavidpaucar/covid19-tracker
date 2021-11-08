d8173616a3b5cec4aaceffd2464e8347638dda3f
David Paucar <soydavidpaucar@gmail.com>
2021-11-07 22:28:12 -0500

    :art: select country dropdown
    created code to put on dropdown the selected country

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 const App = () => {
 	/* Set initial variable values */
 	const [countries, setCountries] = useState([]);
+	const [country, setCountry] = useState(('worldwide'));
 	
 	/* Call api */
 	useEffect(() => {
@@ -12,6 +13,7 @@ const App = () => {
 			await fetch(`https://disease.sh/v3/covid-19/countries`)
 				.then((response) => response.json())
 				.then((data) => {
+					
 					const countries = data.map((country) => {
 						return {
 							name: country.country,
@@ -26,12 +28,19 @@ const App = () => {
 		getCountriesData();
 		
 	}, []);
+	
+	const onCountryChange = (event) => {
+		const countryCode = event.target.value;
+		
+		setCountry(countryCode);
+	};
 	return (
 		<div className="app">
 			<div className="app__header">
 				<h1>COVID-19 Tracker</h1>
 				<FormControl className="app__dropdown">
-					<Select variant="outlined" value="abc">
+					<Select variant="outlined" value={country} onChange={onCountryChange}>
+						<MenuItem value="worldwide">Worldwide</MenuItem>
 						{countries.map((country) => {
 							return (
 								<MenuItem value={country.value}>{country.name}</MenuItem>
