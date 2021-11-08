7fa55cc50925b30a9123d3dc0c8cbd74059b55b2
David Paucar <soydavidpaucar@gmail.com>
2021-11-07 22:18:25 -0500

    :art: api call
    created api call to fetch all countries list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
+import { FormControl, MenuItem, Select } from '@mui/material';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+	/* Set initial variable values */
+	const [countries, setCountries] = useState([]);
+	
+	/* Call api */
+	useEffect(() => {
+		const getCountriesData = async () => {
+			await fetch(`https://disease.sh/v3/covid-19/countries`)
+				.then((response) => response.json())
+				.then((data) => {
+					const countries = data.map((country) => {
+						return {
+							name: country.country,
+							value: country.countryInfo.iso2
+						};
+					});
+					
+					setCountries(countries);
+				});
+		};
+		
+		getCountriesData();
+		
+	}, []);
 	return (
-		<div>
-			<h1>COVID-19 Tracker</h1>
+		<div className="app">
+			<div className="app__header">
+				<h1>COVID-19 Tracker</h1>
+				<FormControl className="app__dropdown">
+					<Select variant="outlined" value="abc">
+						{countries.map((country) => {
+							return (
+								<MenuItem value={country.value}>{country.name}</MenuItem>
+							);
+						})}
+					</Select>
+				</FormControl>
+			</div>
+		
+		
 		</div>
 	);
 };
