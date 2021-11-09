98cd7106e7336a37302ea9adf7456b2216a9a885
David Paucar <soydavidpaucar@gmail.com>
2021-11-08 21:36:19 -0500

    :art: worldwide info
    created code to fetch worldwide info

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ const App = () => {
 	const [country, setCountry] = useState(('worldwide'));
 	const [countryInfo, setCountryInfo] = useState({});
 	
+	useEffect(() => {
+		fetch(`https://disease.sh/v3/covid-19/all`).then(response => response.json()).then(data => {
+			setCountryInfo(data);
+		});
+	}, []);
+	
 	/* Call api */
 	useEffect(() => {
 		const getCountriesData = async () => {
